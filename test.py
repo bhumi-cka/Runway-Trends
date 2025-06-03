12f2070b9eed8cde648bd43ad91329ac31b5efaa
bhumi-cka <95154020+bhumi-cka@users.noreply.github.com>
2025-06-03 12:48:42 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-features=[]
-feature='skirt'
-features_list = feature.split()
-if len(features_list) > 1:
-    features.append(' '.join(features_list[:-1]))
-else:
-    features.append(feature)
-print(features)  
